900099a5fcdff15e9ace60d1f6ba58a9b0c7ab11
Patrick Benedict Lirio <pblirio.1doc@gmail.com>
2025-09-29 20:11:00 +0800

    react setup and counter component

---
+++ react-app/src/components/counter-component/Counter.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const counter = () => {
+  const [count, setCount] = useState(0);
+  //   const [incrementBy, setincrementBy] = useState(1);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    if (count <= 0) {
+      return (count = 0);
+    } else {
+      setCount(count - 1);
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Count value is: {count}</h1>
+        <button type="button" onClick={increment}>
+          Increment
+        </button>
+
+        <button type="button" onClick={decrement}>
+          Decrement
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default counter;
